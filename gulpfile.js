cd5e925b0bd78e2ccc2b51d3ec1b50c3a279963b
Rendalf <ar.lipatov93@yandex.ru>
2016-01-17 11:52:54 +0300

    gulpfile using spaces

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,17 @@ const buffer = require("vinyl-buffer");
 const source = require("vinyl-source-stream");
 
 const config = {
-	bundle: "app.js",
-	srcJs: "src/index.js",
-	distJsPath: "public/js/",
+  bundle: "app.js",
+  srcJs: "src/index.js",
+  distJsPath: "public/js/",
 };
 
 // browserify js
 gulp.task("build", () => {
-	browserify(config.srcJs)
-		.transform(babelify)
-		.bundle()
-		.pipe(source(config.bundle))
-		.pipe(buffer())
-		.pipe(gulp.dest(config.distJsPath));
+  browserify(config.srcJs)
+    .transform(babelify)
+    .bundle()
+    .pipe(source(config.bundle))
+    .pipe(buffer())
+    .pipe(gulp.dest(config.distJsPath));
 });
